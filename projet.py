9cecdaae59ebd3921815c5112df7d1b81c2e79b3
selenaiskounen <60702138+selenaiskounen@users.noreply.github.com>
2020-10-30 15:14:59 +0100

    granger causality test

--- projet.py
+++ projet.py
@@ -21,7 +21,7 @@ import seaborn as sns
 from statsmodels.tsa.stattools import adfuller, acf, pacf
 from statsmodels.tsa.arima_model import ARIMA
 
-# Regression linéaire multiple sur données SIDA:
+# Regression linéaire multiple sur données aids:
 
 df_aids = pd.read_csv("C:/Users/lenovo/Documents/aidscsv")
 df_aids  # chargement des données 
@@ -30,17 +30,26 @@ aids_mixed_all = smf.mixedlm("CD4 ~ obstime+drug+gender+prevOI+AZT", df_aids,
 mdf = aids_mixed_all.fit(method=["lbfgs"])
 print(mdf.summary())  # regression lineaire multiple
 
-# selection des variables pour la regression linéaire simple
-x = df_aids.drop(columns=["Unnamed: 0", "id", "time", "death", "CD4"])  
+# Regression linéaire sur les données aids:
+# Recodage des variables qualitatives:
+d1 = pd.get_dummies(df_aids["drug"])
+d2 = pd.get_dummies(df_aids["gender"])
+d3 =pd.get_dummies(df_aids["prevOI"])
+d4 = pd.get_dummies(df_aids["AZT"])
+# Concaténation successives des nouvelles varibles
+df = pd.concat([df_aids,d1],axis=1)
+df1 = pd.concat([df,d2],axis=1)
+df2 = pd.concat([df1,d3],axis=1)
+df3 = pd.concat([df2,d4],axis=1)
+df_aids = df3
+# selection des variables pour la regression linéaire
+x = df_aids.drop(columns=["Unnamed: 0", "id", "time", "death", "CD4", "drug",
+                          "gender","prevOI","AZT"])  
 y = df_aids["CD4"]
-# création d'échatillon d'entraienemnt et de test
-x_train, x_test, y_train, y_test = train_test_split(x, y,  test_size = 0.2, 
-                                                   random_state = 10)
-
-aids_lm_all = linear_model.LinearRegression()
-aids_lm_all.fit(x_train, y_train)
-print(aids_lm_all.coef_)  # coefficient éstimés par  regression linéaire
-print(aids_lm_all.intercept_)  # intercetpe éstimé par regression linéaire
+# Regression OLS avec statsmodel: 
+model = sm.OLS(y, x)
+results = model.fit()
+print(results.summary())
 df_aids.boxplot('CD4')  # bocplot de la variable à predire
 
 # Regression linéaire classique sur données Boston:
@@ -65,13 +74,26 @@ plt.title("Fréquence des nombres de chambres")
 # Selection des variables prédictives dont on a besoin
 x = df_boston.drop(columns=["MEDV", "ZN", "INDUS", "CHAS", "NOX", "RM", "AGE",
                             "DIS", "RAD", "PTRATIO", "B"])
-
 y = boston.target
-# création d'echantillon d'apprentissage et de test
-x_train, x_test, y_train ,y_test = train_test_split(x, y,  test_size = 0.2, 
-                                                  random_state = 10)
 
-lm = linear_model.LinearRegression()  
-lm.fit(x_train, y_train)
+lm = linear_model.LinearRegression()  # regression linéaire
+lm.fit(x,y)
 print(lm.coef_)  # coefficients etimés par la regression linéaire
-print(lm.intercept_)  # valeur de l'intercepte
+print(lm.intercept_)  # valeur de l'intercept
+
+# Test sur les séries chronologiques données ChickEgg:
+
+df_ce = pd.read_csv("C:/Users/lenovo/Documents/chickeggcsv")
+df_ce.head()  # chargement des données
+
+# representation de la serie chronologique pour les poulets:
+ax = sns.lineplot(x='Unnamed: 0',y='chicken',data=df_ce)
+
+# representation de la serie chronologique des oeufs:
+ax1 = sns.lineplot(x='Unnamed: 0',y='egg',data=df_ce)
+
+#test de granger des valeurs des oeufs a partir de celle des poulets:
+model1 = grangercausalitytests(df_ce[["chicken","egg"]], maxlag=3)
+
+#test de granger des valeurs des poulets à partir de celle des oeufs:
+model2 = grangercausalitytests(df_ce[["egg","chicken"]], maxlag=3)
